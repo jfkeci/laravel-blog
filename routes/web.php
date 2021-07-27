3f3d5893e0b3eef93f7e3bf05eb646e6bc0dfd48
jfkeci <ja.lip132@gmail.com>
2021-07-27 12:50:44 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+/* Route::get('/about', function () {
+    return view('pages.about');
+}); */
+
+/* Route::post('/hello', function () {
+    return "<h1>Hello world</h1>";
+});
+
+Route::get('/users/{name}', function ($name) {
+    return "<h1>Hello world $name </h1>";
+}); */
