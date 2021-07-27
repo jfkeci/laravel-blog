0fb94b070f5d27518cfef8ff14c76ec9948e4da3
jfkeci <ja.lip132@gmail.com>
2021-07-27 15:30:01 +0200

    saving post with a form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,7 @@
 
         <main class="py-4">
             <div class="container">
+                @include('inc.messages')
                 @yield('content')
             </div>
         </main>
