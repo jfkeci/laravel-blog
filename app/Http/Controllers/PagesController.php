ca2c493b8d1e0492fa95567d838153e2bac7cda4
jfkeci <ja.lip132@gmail.com>
2021-07-27 13:33:40 +0200

    laravel ui

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,12 +7,22 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        $title = "welcomme to my blog";
+        return view('pages.index', compact('title'));
     }
     public function about(){
-        return view('pages.about');
+        $title = "welcomme to about";
+        return view('pages.about')->with('title', $title);
     }
     public function services(){
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => [
+                'web design',
+                'programming',
+                'seo'
+            ]
+        );
+        return view('pages.services')->with($data);
     }
 }
